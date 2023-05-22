c8050f28f4352529805eb02cd05b5e60487c734f
jeremiprzeorek <jeremi.przeorek@gmail.com>
2023-05-22 18:56:31 +0200

    Hello there... General Kenobi

---
+++ HT.MauiWorkshop/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HT.MauiWorkshop;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
